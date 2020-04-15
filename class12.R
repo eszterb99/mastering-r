6a5b87f13cc135c25ef190f74be9cb5ae95d4a0c
Eszter <eszter@ip-172-31-26-151.eu-west-1.compute.internal>
2020-04-15 12:50:38 +0000

    tryCatch + new function

--- class12.R
+++ class12.R
@@ -6,7 +6,18 @@ BITCOINS <- 0.42
 log_info('Number of bitcoins {BITCOINS}')
 
 # TODO: binance api need to be added retries
-btcusd <- prices[symbol == 'BTC', usd]
+get_bitcoin_price <- function() {
+  tryCatch(
+    binance_coins_prices()[symbol == 'BTC', usd],
+    error = function(e) get_bitcoin_price()
+  )
+}
+
+get_bitcoin_price()
+
+tryCatch(binance_coins_prices()[symbol == 'BTC', usd])
+
+btcusd <- get_bitcoin_price()
 log_info('The value of one bitcoin is ${btcusd}')
 
 USDHUF <- fromJSON('https://api.exchangeratesapi.io/latest?base=USD&symbols=HUF')$rates$HUF
