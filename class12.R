f078a198d357f09bd43b523a2cd98479dddc96cf
Eszter <eszter@ip-172-31-26-151.eu-west-1.compute.internal>
2020-04-15 12:41:47 +0000

    logging

--- class12.R
+++ class12.R
@@ -1,1 +1,15 @@
 library(binancer)
+library(jsonlite)
+library(logger)
+
+BITCOINS <- 0.42
+log_info('Number of bitcoins {BITCOINS}')
+
+# TODO: binance api need to be added retries
+btcusd <- prices[symbol == 'BTC', usd]
+log_info('The value of one bitcoin is ${btcusd}')
+
+USDHUF <- fromJSON('https://api.exchangeratesapi.io/latest?base=USD&symbols=HUF')$rates$HUF
+log_info('USDHUF exchange rate is {USDHUF}')
+
+btcusd * BITCOINS * USDHUF
